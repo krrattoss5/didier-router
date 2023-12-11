22056365b41feaeacd247a14d96ff022988cf879
krrattoss5 <krrattoss5@gmail.com>
2023-12-11 13:54:40 -0500

    (add/eventListeners)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {EVENTS} from './consts.js'
+
+function navigate(href){
+  window.history.pushState({}, '', href)
+  //CREAR EVENTO PERSONALIZADO
+  const navigationEvent = new Event(EVENTS.PUSHSTATE)
+  //ENVIAR EL EVENTO
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
 
@@ -7,10 +16,12 @@ function HomePage() {
       <h1>Home</h1>
       <p>Esta es una página de ejemplo para crear un React Router desde cero.</p>
       <a href="/about">Ir a sobre nosotros.</a>
+      <br />
+      <button onClick={() => navigate('/about')}>move</button>
     </>
   )
 
-  }
+}
 
 function AboutPage() {
 
@@ -19,12 +30,30 @@ function AboutPage() {
       <h1>About</h1>
       <p>¡Hola! esto es una replica de Reacty Router.</p>
       <a href="/">Ir al Home.</a>
+      <br />
+      <button onClick={() => navigate('/')}>move</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    //escuchar el evento para adelante
+    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
+    //escuchar el evento atraz, popstate es el evento de retorceso
+    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
+
+    //es importante depurar los eventos siempre para que no se sobrecarguen de listeners(esta logica de depuracion es apropiada)
+    return () => {
+      removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
+      removeEventListener(EVENTS.POPSTATE, onLocationChange)
+    }
+  },[])
 
   return (
     <main>
